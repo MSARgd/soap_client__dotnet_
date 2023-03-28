138e0c6aac98138ab6f18aa3ea378b0ef49fe70a
sidiAbdalla <sidabdallard@gmail.com>
2023-03-28 22:09:29 +0000

    date

--- soap/ClientBanqueSoap.cs
+++ soap/ClientBanqueSoap.cs
@@ -2,5 +2,12 @@ namespace soap_client_dotnet.soap;
 
 public class ClientBanqueSoap
 {
+    class program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
     
 }
