8e1642d11bcebd61000b80b148fad91e10e6da6b
sidiAbdalla <sidabdallard@gmail.com>
2023-03-28 22:54:17 +0000

    --date

--- soap/ClientBanqueSoap.cs
+++ soap/ClientBanqueSoap.cs
@@ -1,13 +1,23 @@
+using soap_client_dotnet.BanqueWSService;
 namespace soap_client_dotnet.soap;
-
 public class ClientBanqueSoap
 {
     class program
     {
         static void Main(string[] args)
         {
-            
+            /*=========================================*/
+            BanqueWSService.BanqueWSClient stub = new BanqueWSClient();
+            double result = stub.ConversationEuroToDhAsync(100).Result.@return;
+            Console.WriteLine(result);
+            /* ==========================================  */
+            compte compte = stub.getCompteAsync(1).Result.@return;
+            Console.Write("Compte :  ---->");
+            Console.WriteLine("code  : "+compte.code+"solde : "+compte.solde);
+            /*========================================= */
+
+            /*================================================*/
+            Console.ReadLine();
         }
     }
-    
 }
